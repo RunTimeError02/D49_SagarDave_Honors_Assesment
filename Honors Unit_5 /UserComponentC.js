2a807e19bc38622769ef0ae74df1f260f90ce3d8
Sagar Dave <102181573+RunTimeError02@users.noreply.github.com>
2024-04-04 10:53:19 +0530

    Create UserComponentC.js

---
+++ Honors Unit_5 /UserComponentC.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import ComponentD from './ComponentD';
+
+
+export class ComponentC extends Component{
+  render(){
+    return (
+      <ComponentD/>
+    )
+  }
+}
+
+export default ComponentC;
